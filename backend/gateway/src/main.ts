4a9c7bd84cf2d367ade00635d50de1f53a43dc3c
Heba-Webdev23 <jtvonline@yahoo.com>
2025-01-14 22:19:06 +0300

    feat: global prefix and global pipes config

--- backend/gateway/src/main.ts
+++ backend/gateway/src/main.ts
@@ -2,28 +2,40 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigEnvs } from './config/envs';
 import * as cookieParser from 'cookie-parser';
-import { AuthMiddleware } from './middleware/auth.middleware';
-import { JwtService } from '@nestjs/jwt';
-import { Logger, LogLevel } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { Logger, LogLevel, RequestMethod, ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+dotenv.config();  
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logLevels: LogLevel[] = ['log', 'error', 'warn', 'debug', 'verbose'];
   app.useLogger(logLevels);
+  app.setGlobalPrefix('api', {
+    exclude: [{path: '/', method: RequestMethod.GET}]
+  });
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true
+  }));
   app.enableCors({
     origin: '*',
     credentials: true,
     methods: 'GET,HEAD,POST,PUT,DELETE,OPTIONS',
   });
   app.use(cookieParser());
-  const jwtService = app.get(JwtService);
-  const authMiddleware = new AuthMiddleware(jwtService);
-  app.use(authMiddleware.use.bind(authMiddleware));
+  const config = new DocumentBuilder()
+    .setTitle('IUPI API Documentation')
+    .setDescription('IUPI, Ahorra e inversiones')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   const port = ConfigEnvs.PORT
   await app.listen(port,()=> {
     Logger.log(`Application is running on: http://localhost:${port}`);
   });
-  
 }
 bootstrap().catch((err) => {
   Logger.log('Global error handler');
