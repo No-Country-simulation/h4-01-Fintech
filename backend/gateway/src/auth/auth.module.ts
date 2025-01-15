c936c07731c24d3c1d253621e6dd29e93c2b8f3c
Heba-Webdev23 <jtvonline@yahoo.com>
2025-01-15 16:19:01 +0300

    feat: login with an email and a password

--- backend/gateway/src/auth/auth.module.ts
+++ backend/gateway/src/auth/auth.module.ts
@@ -1,23 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { JwtModule } from '@nestjs/jwt';
-import { UserService } from '../users/user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from '../entitys/user.entity';
 import { AccountEntity } from 'src/entitys/account.entity';
-import { ConfigEnvs } from 'src/config/envs';
+import { JwtService } from '@nestjs/jwt';
+import { UsersModule } from 'src/users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    UsersModule,
+    ConfigModule,
     TypeOrmModule.forFeature([UserEntity, AccountEntity]),
-    JwtModule.register({
-      secret: ConfigEnvs.JWT_SECRET as string,
-      signOptions: { expiresIn: '1h' },
-    }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, UserService],
-  exports: [AuthService, UserService],
+  providers: [AuthService, JwtService],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- backend/gateway/src/auth/auth.service.ts
+++ backend/gateway/src/auth/auth.service.ts
@@ -1,11 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserEntity } from '../entitys/user.entity';
-import { AccountEntity } from '../entitys/account.entity';
+import { UserService } from 'src/users/user.service';
+import { LoginWithCredentialsDto } from './dto/login-credentials.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private userService: UserService,
+    private configService: ConfigService
+  ) {}
+
+  async login(dto: LoginWithCredentialsDto) {
+        const user = await this.userService.findByEmail(dto.email);
+        if (!user) throw new UnauthorizedException('Invalid credenitals');
+        const payload = {
+            email: user.email,
+            sub: user.id
+        };
+      const secret = this.configService.get<string>('JWT_SECRET');
+
+      const token = await this.jwtService.signAsync(payload, {
+        secret: secret
+      });
+
+      return {
+        message: 'Incio de sesión exitoso',
+        access_token: token,
+        data: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+        }
+      };
+  }
 
   generateJwtToken(user: UserEntity): string {
     const payload = {
