cfec7e825d25576e7735042cb6447024ec5ffe70
dewinson1 <dewinson12@gmail.com>
2025-01-18 09:41:27 -0400

    fix1

--- front/src/app/components/organisms/LeftSection.tsx
+++ front/src/app/components/organisms/LeftSection.tsx
@@ -1,18 +1,25 @@
 export default function LeftSection() {
-    return <div className="hidden lg:flex lg:w-2/5 bg-[#0052CC] text-white p-12 flex-col justify-between">
-    <div className="space-y-6">
-      <h1 className="text-4xl font-bold leading-tight">
-        Facilitamos Tu Ahorro E Inversión
-      </h1>
-      <h2 className="text-xl">
-        Haz crecer tu dinero con estrategias personalizadas
-      </h2>
-      <p className="text-sm opacity-90">
-        Define tus metas financieras y comienza a invertir hoy mismo, sin complicaciones.
-      </p>
+  return (
+    <div className="hidden lg:flex lg:w-2/5 bg-[#0052CC] text-white p-12 flex-col justify-center">
+    
+      <div className="space-y-6">
+        <h1 className="text-4xl font-bold leading-tight text-left">
+          Facilitamos Tu
+          <br /> Ahorro E <br />
+          Inversión
+        </h1>
+        <h2 className="text-xl">
+          Haz crecer tu dinero con estrategias personalizadas
+        </h2>
+        <p className="text-lg  opacity-90">
+          Define tus metas financieras y comienza a invertir hoy mismo, sin complicaciones.
+        </p>
+      </div>
+
+     
+      <div className="text-2xl font-bold mt-16">
+        iUpi
+      </div>
     </div>
-    <div className="text-2xl font-bold">
-      iUpi
-    </div>
-  </div>
+  );
 }
