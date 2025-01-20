0de91f0493aa955b3b587d2012c3b7eb868a6608
orlando0107 <orlandocv0107@gmail.com>
2025-01-20 07:38:28 -0300

    remove: case-sensitive

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,14 +1,6 @@
-import type { NextConfig } from "next";
-const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer, dev }) => {
-    // Solo aplica el plugin en desarrollo
-    if (dev) {
-      config.plugins.push(new CaseSensitivePathsPlugin());
-    }
-    return config;
-  },
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
