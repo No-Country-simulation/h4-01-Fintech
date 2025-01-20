6e2be295d3b418a58fdd7b1c0c6439534be54635
orlando0107 <orlandocv0107@gmail.com>
2025-01-20 07:56:47 -0300

    Test. nex.conft

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,8 +1,16 @@
 import { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com"],
+  },
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "src"),
+    };
+    return config;
   },
 };
 
