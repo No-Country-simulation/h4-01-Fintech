7d2eb52307ea0c8f8bb70ff8fb937d5e47d42b9c
orlando0107 <orlandocv0107@gmail.com>
2025-01-20 07:48:25 -0300

    fix: avatar

--- front/src/app/components/atoms/useravatar.tsx
+++ front/src/app/components/atoms/useravatar.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useSession } from "next-auth/react";
 // import Image from "next/image";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/src/components/ui/avatar";
 
 
 export default function AvatarUser() {
