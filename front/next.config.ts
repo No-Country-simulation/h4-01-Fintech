15c151edfc6fe4d21832b93689f7bee5ddaafc17
orlando0107 <orlandocv0107@gmail.com>
2025-01-20 13:31:14 -0300

    test: #2 deploy

--- front/next.config.ts
+++ front/next.config.ts
@@ -3,16 +3,6 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   basePath: "/",
-  experimental: {
-    dynamicIO: true,
-    cacheLife: {
-      blog: {
-        stale: 3600, // 1 hour
-        revalidate: 900, // 15 minutes
-        expire: 86400, // 1 day
-      },
-    },
-  },
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
