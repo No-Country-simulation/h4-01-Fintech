ca16f8c696adcdedd5e14abba031cb98233cde23
orlando0107 <orlandocv0107@gmail.com>
2025-01-20 07:25:56 -0300

    add: config eslint++

--- front/eslint.config.mjs
+++ front/eslint.config.mjs
@@ -5,6 +5,7 @@ import tsParser from "@typescript-eslint/parser";
 import pluginReact from "eslint-plugin-react";
 import nextConfig from "eslint-config-next";
 import importPlugin from "eslint-plugin-import";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
@@ -28,16 +29,37 @@ export default [
       },
     },
     plugins: {
+      simpleImportSort,
+      prettier: "prettier",
       "@typescript-eslint": tseslint,
       react: pluginReact,
       import: importPlugin,
     },
+    extends: [
+      "plugin:prettier/recommended", // Asegura que Prettier esté activado
+      "eslint-config-prettier", // Desactiva las reglas de ESLint que podrían entrar en conflicto con Prettier
+      "plugin:@typescript-eslint/recommended",
+      "plugin:react/recommended",
+      "plugin:import/recommended",
+    ],
     rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          singleQuote: true,
+          semi: false,
+          trailingComma: "es5",
+          tabWidth: 2,
+        },
+      ],
       // Reglas generales
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
       "no-unused-vars": "error",
       "no-undef": "error",
       "no-console": "error",
       "no-debugger": "error",
+      "import/no-unresolved": "error",
 
       // Reglas de TypeScript
       "@typescript-eslint/no-unused-vars": ["error"],
