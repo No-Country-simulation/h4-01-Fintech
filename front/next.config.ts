3eda510e5538d20679ad661c43dfbdb38c1f6160
orlando0107 <orlandocv0107@gmail.com>
2025-01-20 13:44:21 -0300

    test : 3

--- front/next.config.ts
+++ front/next.config.ts
@@ -2,16 +2,8 @@ import { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  basePath: "/",
   images: {
     domains: ["lh3.googleusercontent.com"],
-  },
-  webpack(config) {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.resolve(__dirname, "src"),
-    };
-    return config;
   },
 };
 
