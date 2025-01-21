324138129e45e1ade41b49c21628d0348ef18333
Heba-Webdev23 <jtvonline@yahoo.com>
2025-01-21 18:13:25 +0300

    feat: questions min and max ranges

--- backend/gateway/src/questions/dto/add-question.dto.ts
+++ backend/gateway/src/questions/dto/add-question.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsNumber, Min } from "class-validator";
+import { IsNotEmpty, IsNumber, IsString, Max, Min } from "class-validator";
 import { ApiProperty } from "@nestjs/swagger";
 
 export class QuestionDto {
@@ -19,4 +19,40 @@ export class QuestionDto {
     @IsNumber()
     @Min(1)
     order: number;
+
+    @ApiProperty({
+    example: 1,
+    description: 'Minimum range value',
+    default: 1
+     })
+    @IsNumber()
+    @Min(1)
+    minRange: number = 1;
+
+    @ApiProperty({
+    example: 10,
+    description: 'Maximum range value',
+    default: 10
+    })
+    @IsNumber()
+    @Max(10)
+    maxRange: number = 10;
+
+    @ApiProperty({
+    example: 'Not at all likely',
+    description: 'Label for minimum range value',
+    nullable: false
+    })
+    @IsNotEmpty()
+    @IsString()
+    minRangeLabel: string;
+
+    @ApiProperty({
+    example: 'Extremely likely',
+    description: 'Label for maximum range value',
+    nullable: false
+    })
+    @IsNotEmpty()
+    @IsString()
+    maxRangeLabel: string;
 }
