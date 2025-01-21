e81c007bc58506614266d8467ef1238ed820194c
orlando0107 <orlandocv0107@gmail.com>
2025-01-21 18:32:38 -0300

    fix: delete front

--- front/src/app/auth/login/page.tsx
+++
@@ -1,9 +0,0 @@
-
-import Login from '@/components/views/login/log'
-import React from 'react'
-
-export default function Page() {
-  return (
-      <><Login/></>
-  )
-}
--- front/src/app/error/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function pague() {
-  return <>Oops</>
-}
