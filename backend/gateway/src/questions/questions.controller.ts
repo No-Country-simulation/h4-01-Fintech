6a5c674c1e585217516eda4d0f8b6b6c13be6499
Heba-Webdev23 <jtvonline@yahoo.com>
2025-01-22 18:12:57 +0300

    feat: update question endpoint

--- backend/gateway/src/questions/questions.controller.ts
+++ backend/gateway/src/questions/questions.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UseGuards } from '@nestjs/common';
 import { QuestionsService } from './questions.service';
 import { QuestionDto } from './dto/add-question.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
@@ -7,6 +7,7 @@ import { Roles } from 'src/rbac/metadata/role.metadata';
 import { Role } from 'src/rbac/roles';
 import { ApiTags } from '@nestjs/swagger';
 import { GetQuestionsDto } from './dto/get-questions.dto';
+import { UpdateQuestionDto } from './dto/update-question.dto';
 
 @ApiTags('questions')
 @Controller('questions')
@@ -31,5 +32,12 @@ export class QuestionsController {
   @Delete('/:id')
   async deleteQuestion(@Param('id') id: string, @Query('isDeleted') isDeleted: string) {
     return this.questionsService.deleteQuestion(id, isDeleted);
+  }
+
+  @UseGuards(AuthGuard, RoleGuard)
+  @Roles(Role.MODERATOR, Role.ADMIN)
+  @Patch('/:id')
+  async updateQuestion(@Param('id') id: string, @Body() dto: UpdateQuestionDto) {
+    return this.questionsService.updateQuestion(id, dto);
   }
 }
