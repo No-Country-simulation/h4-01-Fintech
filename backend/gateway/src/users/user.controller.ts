65b09c00d2d15ac23ff2feafe5e142f40688da55
spimentel1201 <spimentelm1201@gmail.com>
2025-01-23 14:11:48 -0500

    fixes email verification

--- backend/gateway/src/users/user.controller.ts
+++ backend/gateway/src/users/user.controller.ts
@@ -2,19 +2,22 @@ import {
     Controller,
     Get,
     Request,
+    UseGuards,
   } from '@nestjs/common';
   import { UserService } from './user.service';
   import {
     ApiBearerAuth,
     ApiTags
 } from '@nestjs/swagger';
+import { AuthGuard } from 'src/auth/auth.guard';
 
-@ApiTags('Users')
+@ApiTags('users')
 @ApiBearerAuth()
 @Controller('users')
 export class UserController {
     constructor(private usersService: UserService) {}
 
+    @UseGuards(AuthGuard)
     @Get('profile')
     getProfile(@Request() req) {
         return this.usersService.getFullProfile(req.user.userId);
