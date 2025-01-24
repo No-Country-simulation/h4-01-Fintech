a538439a0b620f1cf3d83bd9640ad4563212868d
Heba-Webdev23 <jtvonline@yahoo.com>
2025-01-24 21:41:39 +0300

    feat: extract user id from jwt token

--- backend/gateway/src/answers/answers.controller.ts
+++ backend/gateway/src/answers/answers.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, Req, UseGuards } from '@nestjs/common';
 import { AnswersService } from './answers.service';
 import { AddAnswerDto } from './dto/add-answer.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { AuthenticatedRequest } from 'src/middleware/auth.middleware';
 
 @Controller('answers')
 export class AnswersController {
@@ -10,7 +11,8 @@ export class AnswersController {
   @UseGuards(AuthGuard)
   @HttpCode(HttpStatus.OK)
   @Post()
-  async add(@Body() dto: AddAnswerDto) {
-    return await this.answersService.addAnswer(dto);
+  async add(@Req() req: AuthenticatedRequest, @Body() dto: AddAnswerDto) {
+    const userId = req.user['id'];
+    return await this.answersService.addAnswer(userId, dto);
   }
 }
