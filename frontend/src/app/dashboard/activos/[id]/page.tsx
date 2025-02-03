e677c501ad472ff9461be1c053f4a2a9576037a5
orlando0107 <orlandocv0107@gmail.com>
2025-02-03 11:15:06 -0300

    fix:page

--- frontend/src/app/dashboard/activos/[id]/page.tsx
+++ frontend/src/app/dashboard/activos/[id]/page.tsx
@@ -1,8 +1,11 @@
 import { Flex } from '@radix-ui/themes'
 import React from 'react'
 
-
 export default function Page({ params }: { params: { id: string } }) {
+  if (!params || typeof params.id !== 'string') {
+    return <div>Error: Parámetro no válido</div>
+  }
+
   return (
     <Flex className='h-[100vh]'>
       <div className='p-2 m-2 justify-center items-center'>
