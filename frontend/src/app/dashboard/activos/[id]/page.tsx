f2b14f96c38676ccf745850c0ec6acfc97147a55
orlando0107 <orlandocv0107@gmail.com>
2025-02-03 10:55:54 -0300

    fix:ruta dinamica

--- frontend/src/app/dashboard/activos/[id]/page.tsx
+++ frontend/src/app/dashboard/activos/[id]/page.tsx
@@ -1,14 +1,16 @@
 import { Flex } from '@radix-ui/themes'
 import React from 'react'
 
-interface Params {
-    params: {
-        id: string
-    }
+interface PageProps {
+  params: { id: string }
 }
 
-export default function page({ params }: Params) {
+export default function Page({ params }: PageProps) {
   return (
-    <Flex className='h-[100vh]'><div className='p-2 m-2 justify-center justfy-items-center'>Activo : {params.id}</div></Flex>
+    <Flex className='h-[100vh]'>
+      <div className='p-2 m-2 justify-center justfy-items-center'>
+        Activo : {params.id}
+      </div>
+    </Flex>
   )
 }
